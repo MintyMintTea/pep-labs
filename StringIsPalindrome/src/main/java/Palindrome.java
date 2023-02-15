1fccb0293f558f7d51aa60610d3a1aee7f32bf9b
MintyMintTea <sejr00@icloud.com>
2023-02-15 19:39:48 +0000

    Saved progress on StringIsPalindrome 2023-02-15 19:39:48 +0000

--- StringIsPalindrome/src/main/java/Palindrome.java
+++ StringIsPalindrome/src/main/java/Palindrome.java
@@ -12,6 +12,13 @@ public class Palindrome {
      * @return true if str is a palindrome, false otherwise.
      */
     public boolean pal(String str){
+        String pString = "";
+        for(int i = str.length()-1; i >=0;i--){
+            pString = pString+str.charAt(i);
+        }
+        if(str.equals(pString)){
+            return true;
+        }
         return false;
     }
 }
